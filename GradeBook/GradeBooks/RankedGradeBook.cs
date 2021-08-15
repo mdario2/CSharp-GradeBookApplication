40b0bf49c3cda1c0001453e80b861dfea7acb61a
Manuel Dario Gallardo <mdariogallardo@gmail.com>
2021-08-15 01:15:57 -0500

    Validation Range

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,23 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+
+
+            if (averageGrade <= 20)
+                return 'A';
+            else if (averageGrade > 20 && averageGrade < 40)
+                        return 'B';
+            else if (averageGrade > 40 && averageGrade < 60)
+                        return 'C';
+           else if (averageGrade > 60 && averageGrade < 80)
+                        return 'D';
+           else
+                return 'F';
+
+
+        }
     }
 }
